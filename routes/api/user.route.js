6cef56f05e3f8e257cb08dee6a0220bbf3c04a4d
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2022-06-24 16:10:27 -0300

    .

--- routes/api/user.route.js
+++ routes/api/user.route.js
@@ -5,29 +5,24 @@ var UploadController = require('../../controllers/upload.controller');
 var MailController = require('../../controllers/mail.controller');
 var Authorization = require('../../auth/authorization');
 
-
 // Authorize each API with middleware and map to the Controller Functions
 /* GET users listing. */
-router.get('/test', function(req, res, next) {
-    res.send('Llegaste a la ruta de  api/user.routes');
-  });
+router.get('/test', function (req, res, next) {
+  res.send('Llegaste a la ruta de  api/user.routes');
+});
 router.post('/registration', UserController.createUser)
 router.post('/login/', UserController.loginUser)
-router.get('/',Authorization, UserController.getUsers)
+router.get('/', Authorization, UserController.getUsers)
 router.post('/userByMail', Authorization, UserController.getUsersByMail)
 router.put('/', Authorization, UserController.updateUser)
 router.delete('/:id', Authorization, UserController.removeUser)
-router.post('/guardarImgUser',UserController.guardarImagenUser)
-router.post('/uploadImg',UploadController.uploadFilesImgUser);
-router.post('/imgUserByMail',Authorization,UserController.getImagenUserByMail)
-router.post('/sendMail',MailController.sendEmail)
-
-
+router.post('/guardarImgUser', UserController.guardarImagenUser)
+router.post('/uploadImg', UploadController.uploadFilesImgUser);
+router.post('/imgUserByMail', Authorization, UserController.getImagenUserByMail)
+router.post('/sendMail', MailController.sendEmail)
 
 // Export the Router
 module.exports = router;
-
-
 
 //api/users
 //api/users/registration
