a367b034b39554538489c56ba0501eca91fb557e
Mauro Rodriguez <rodriguezmauro1994@gmail.com>
2022-07-14 20:19:28 -0300

    removed difficulty

--- routes/api.js
+++ routes/api.js
@@ -5,11 +5,9 @@ var router = express.Router();
 var users = require('./api/user.route');
 var recipes = require('./api/recipe.route');
 var categories = require('./api/category.route');
-var difficulty = require('./api/difficulty.route');
 
 router.use('/users', users);
 router.use('/recipes', recipes);
 router.use('/categories', categories);
-router.use('/difficulties', difficulty);
 
 module.exports = router;
