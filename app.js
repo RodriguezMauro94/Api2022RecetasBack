c2cdc94ca712f820aa7e89d1d0d8ebd205dc0abd
Franco Benito <francobenito@protonmail.com>
2022-07-14 22:31:22 -0300

    Update app.js
    cors config

--- app.js
+++ app.js
@@ -26,7 +26,9 @@ app.use(express.urlencoded({
 }));
 
 //aplico cors
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
